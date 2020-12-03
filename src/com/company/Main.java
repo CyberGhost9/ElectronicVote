d63cae63f7219d13ca38c1c44c94555da3a30827
Rajat Kanti Biswas <74067540+ConfusedRobo@users.noreply.github.com>
2020-12-03 21:04:24 +0530

    This is a change made by Rajat Kanti Biswas.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,20 @@
 package com.company;
 
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter Number Of Political Parties Allowed:");
-        int limit=sc.nextInt();
-        new PoliticalParties(limit);
-        System.out.print("Enter Voters Limit: ");
-        sc.nextLine();
-        int capacity =sc.nextInt();
-        new Citizen(capacity);
-        sc.close();
-	}
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter party capacity: ");
+        int partyLimit = scanner.nextInt();
+        new PoliticalParties(partyLimit);
+
+        System.out.print("Enter voters capacity: ");
+        int voterLimit = scanner.nextInt();
+        new Citizen(voterLimit);
+
+        scanner.close();
+    }
 }
