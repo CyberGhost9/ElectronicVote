e80ec66d26243fb421a0c2ae17745ac53a4bfdc6
Rajat Kanti Biswas <74067540+ConfusedRobo@users.noreply.github.com>
2020-12-06 11:25:12 +0530

    Added a method. And alt line 25 it will recursively call itself if the limit is invalid.

--- src/com/company/Citizen.java
+++ src/com/company/Citizen.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class Citizen {
 
     public Citizen(int limit) {
+        citizenProperties(limit);
+    }
+
+    public void citizenProperties(int limit) {
         Scanner scanner = new Scanner(System.in);
         String[] citizenName = new String[limit];
         final int currentYear = 2020;
         int[] id = new int[limit];
-
 
         for (int i = 0; i < limit; i++) {
             System.out.print("Enter year of birth: ");
@@ -19,7 +22,7 @@ public class Citizen {
             scanner.nextLine();
             if (age < 18) {
                 System.out.println("Not Eligible For Voting");
-                return;
+                citizenProperties(limit);
             } else {
                 System.out.print("\tEnter Name: ");
                 citizenName[i] = scanner.nextLine();
